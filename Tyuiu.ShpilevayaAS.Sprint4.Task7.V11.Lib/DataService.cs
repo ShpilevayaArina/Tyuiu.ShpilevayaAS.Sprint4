cf7475f36fd680fff16116b66c1cf4cc7447b473
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-11-11 21:54:22 +0500

    Task7

---
+++ Tyuiu.ShpilevayaAS.Sprint4.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ShpilevayaAS.Sprint4.Task7.V11.Lib
+{
+    public class DataService : ISprint4Task7V11
+    {
+        public int Calculate(int rows, int columns, string value)
+        {
+            int[,] mtrx = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    mtrx[i, j] = int.Parse(value.Substring(i * columns + j, 1));
+                }
+            }
+
+            int count = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (mtrx[i,j]%2 != 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.ShpilevayaAS.Sprint4.Task7.V11/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShpilevayaAS.Sprint4.Task7.V11.Lib;
+
+namespace Tyuiu.ShpilevayaAS.Sprint4.Task7.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int rows = 4;
+            int columns = 2;
+            int[,] mtrx = new int[rows, columns];
+            string str = "56789012";
+            int index = 0;
+
+            Console.Title = "Спринт #4 | Выполнила: Шпилевая А. С. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант 11                                                              *");
+            Console.WriteLine("* Выполнила: Шпилевая Арина Станиславовна | ИИПб-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр '56789012'.                           *");
+            Console.WriteLine("* Преобразуйте ее в матрицу 4 на 2 и подсчитайте количество               *");
+            Console.WriteLine("* нечетных чисел в матрице.                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]}\t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+
+            int res = ds.Calculate(rows, columns, str);
+            Console.WriteLine("Количество нечетных чисел в матрице = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
